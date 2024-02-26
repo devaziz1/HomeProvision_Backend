4882591aaeaf2427a0a6040d82d9a8fdde5bf6a3
devaziz1 <devaziz1@gmail.com>
2024-02-26 21:33:12 +0500

    Make some changes

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -15,32 +15,31 @@ app.use(express.json());
 
 router.post("/signup", async (req, res, next) => {
   try {
-    const {name, email, password, phone, address } = req.body;
-    console.log(req.body);
+    const { name, email, password, phone, address } = req.body;
+      const hashedPassword = await argon2.hash(password);
 
-    const hashedPassword = await argon2.hash(password);
+    const existingUser = await User.findOne({ email });
 
-      const existingUser = await User.findOne({ email });
-
-      if(existingUser){
-       return res.status(401).json({
-         message: "User with this email already exists",
-       });
-      }
-
-      const newUser = new User({
-        name,
-        password: hashedPassword,
-        email,
-        address,
-        phone,
+    if (existingUser) {
+      return res.status(401).json({
+        message: "Email Already Exists",
       });
+    }
 
-      const result = await newUser.save();
-   return res.status(200).json({
-     message: "inside this API",
-     result,
-   });
+    const newUser = new User({
+      name,
+      password: hashedPassword,
+      email,
+      address,
+      phone,
+    });
+
+    const result = await newUser.save();
+    return res.status(200).json({
+      message: "User saved successfully",
+      name,
+      result,
+    });
   } catch (error) {
     if (!error.statusCode) {
       error.statusCode = 500;
@@ -50,40 +49,37 @@ router.post("/signup", async (req, res, next) => {
 });
 
 router.post("/login", async (req, res, next) => {
-    try {
-      console.log(req.body);
+  try {
+    console.log(req.body);
 
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
-      
-
-      if (!user) {
+    if (!user) {
       return res.status(401).json({
         message: "Invalid Email Address",
       });
-      } else {
-        const passwordMatches = await argon2.verify(user.password, password);
+    } else {
+      const passwordMatches = await argon2.verify(user.password, password);
 
-        if (!passwordMatches) {
-         return res.status(401).json({
-           message: "Invalid Password",
-         });
-        }
-        const name = user.name;
-       return res.status(200).json({
-          email,
-          name,
-          message: "user Login sucessfull ",
+      if (!passwordMatches) {
+        return res.status(401).json({
+          message: "Invalid Password",
         });
       }
-    } catch (error) {
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
+      const name = user.name;
+      return res.status(200).json({
+        email,
+        name,
+        message: "user Login sucessfull ",
+      });
     }
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
   }
-);
+});
 
 module.exports = router;
